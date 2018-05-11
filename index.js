051a69d41e600a1f94e0b3b1d3d3dbf9042e9f55
Sreepal Reddy K <39093045+ksreepal@users.noreply.github.com>
2018-05-11 12:14:42 +0530

    Update index.js
    using JSON.stringify(statusCode);

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ restService.post('/v2/webhook',(req,res)=>{
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
-  response = response.statusCode;
+  response = JSON.stringify(response.statusCode) ;
 });
    
    /*
