72c740f2005e72c4c31291a82252f0d6105d4480
Sreepal Reddy K <39093045+ksreepal@users.noreply.github.com>
2018-09-03 12:25:35 +0530

    Update index.js
    test with app permissions

--- index.js
+++ index.js
@@ -28,10 +28,33 @@ restService.post('/v2/webhook',(req,res)=>{
 }else{
   
   var action = req.body.queryResult.action;
- 
   
    if(action === 'second.action'){
-     response = "Your are in Second Intent from Git Hub code..."
+     
+     //response = "Your are in Second Intent from Git Hub code..."
+     const requestPermission = (app) => {
+     app.askForPermission('To locate you', app.SupportedPermissions.DEVICE_PRECISE_LOCATION);
+     };
+     
+     const userInfo = (app) => {
+        if (app.isPermissionGranted()) {
+            const address = app.getDeviceLocation().address;
+            if (address) {            
+                app.tell('You are at ${address}');
+            }
+            else {
+                // Note: Currently, precise locaton only returns lat/lng coordinates on phones and lat/lng coordinates 
+                // and a geocoded address on voice-activated speakers. 
+                // Coarse location only works on voice-activated speakers.
+                app.tell('Sorry, I could not figure out where you are.');
+            }
+        } else {
+            app.tell('Sorry, I could not figure out where you are.');
+        }
+    };
+     
+     
+     
    }
   
  else if(action === 'input.help'){
